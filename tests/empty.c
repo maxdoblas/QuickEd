33c217fbc4d4553af609e06ca53c45ef6ae6e98b
Oscar Lostes Cazorla <oscar.lostes@bsc.es>
2024-02-21 16:37:20 +0100

    Separate Example test + New test

---
+++ tests/empty.c
@@ -0,0 +1,39 @@
+/*
+ *                             The MIT License
+ *
+ * This file is part of QuickEdit library.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <quicked.h>
+
+int main(void) {
+    // TODO: Enable when error handling for this case is implemented
+    // quicked_aligner_t aligner;
+    // quicked_params_t params = quicked_default_params();
+
+    // quicked_new(&aligner, &params);
+
+    // quicked_align(&aligner, "", 0, "", 0);
+
+    // quicked_free(&aligner);
+
+    return 0;
+}
