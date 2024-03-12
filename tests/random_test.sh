e5ef5fe6535e707e28d4a7744d326cca90151777
Oscar Lostes Cazorla <oscar.lostes@bsc.es>
2024-03-12 16:54:21 +0100

    Add tests for align_benchmark

--- tests/random_test.sh
+++ tests/random_test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # This test generate N random sequence pairs of length L and an average error E using generate_dataset tool,
-#   and pass them to quicked_harness.
-# This test does not check for correctness, only for crashes.
+#   and pass them to quicked_harness or to align_benchmark.
+# quicked_harness does not check for correctness, only for crashes.
 
 # --- Cleanup ---
 
@@ -18,6 +18,7 @@ trap cleanup EXIT INT
 L=$1
 N=$2
 E=$3
+tool=$4
 
 if [ -z "$N" ]; then
     N=10
@@ -41,17 +42,21 @@ echo "Trimming sequences"
 # Remove the first character of each line in random_dataset.seq
 sed -i 's/^.//' "$tempdir/random_dataset.seq"
 
-# For each sequence pair in random_dataset.seq, run ./quicked_harness seq1 seq2
-# If quicked_harness crashes, abort the test with a non-zero exit code
-echo "Running quicked_harness"
-while read -r seq1 && read -r seq2; do
-    # echo "Running quicked_harness with sequences:"
-    # echo "> $seq1"
-    # echo "< $seq2"
-    # printf "\n"
+if [ "$tool" = "quicked_harness" ]; then
+    # For each sequence pair in random_dataset.seq, run ./quicked_harness seq1 seq2
+    # If quicked_harness crashes, abort the test with a non-zero exit code
+    echo "Running quicked_harness"
+    while read -r seq1 && read -r seq2; do
+        # echo "Running quicked_harness with sequences:"
+        # echo "> $seq1"
+        # echo "< $seq2"
+        # printf "\n"
 
-    if ! "$BIN_DIR"/quicked_harness "$seq1" "$seq2"; then
-        echo "quicked_harness crashed"
-        exit 1
-    fi
-done < "$tempdir/random_dataset.seq"
+        if ! "$BIN_DIR"/quicked_harness "$seq1" "$seq2"; then
+            echo "quicked_harness crashed"
+            exit 1
+        fi
+    done < "$tempdir/random_dataset.seq"
+elif [ "$tool" = "align_benchmark" ]; then
+    "$BIN_DIR"/align_benchmark -i "$tempdir/random_dataset.seq" -c 'score' -v
+fi
---
+++ tests/test_data/README.md
@@ -0,0 +1,7 @@
+# Test data
+
+## *ONT.MiniION.1.seq*
+
+This file contains a randomly selected pair of sequences from the ONT MinION dataset [Bowden et al. (2019)].
+
+The average error of the dataset is 7.4% and the sequence is at least 500 kbp long.
